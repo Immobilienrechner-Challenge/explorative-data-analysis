9645a047f79c9be9c57b6f30877d8652dd7486b7
Gabriel Torres Gamez <gabriel.torres@fuet.ch>
2022-10-11 16:43:09 +0200

    change to helper

--- helper.py
+++ helper.py
@@ -1,15 +1,18 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
-
+from sklearn.preprocessing import Normalizer
 
 class ImmoHelper(object):
     def __init__(self, url="https://raw.githubusercontent.com/Immobilienrechner-Challenge/data/main/immoscout_cleaned_lat_lon_fixed_v9.csv", type="csv"):
+        self.X = None
+        self.y = None
         # Erweiterbar für andere Dateitypen
         if type == "csv":
             self.data = pd.read_csv(url, low_memory=False)
 
-    def process_data(self):
-        data = self.data.copy()
+    def process_data(self, data = None):
+        if data == None:
+            data = self.data.copy()
+
         # Nur relevante Spalten selektieren
         cols = [
             "price_cleaned",
@@ -22,8 +25,15 @@ class ImmoHelper(object):
 
         # Relevante Spalten verarbeiten
         data["Living space"] = data["Living space"].str.replace("m²", "").astype(float)
-        data = pd.get_dummies(data["type"])
+        data = pd.get_dummies(data, columns=["type"])
         data = data.dropna()
 
-        self.data = data.copy()
-        return self.data
+        # X und y definieren und X normalisieren
+        y = data["price_cleaned"].values
+        X = data.drop(columns = ["price_cleaned"]).values
+        X = Normalizer().transform(X)
+
+        self.data = data
+        self.X = X
+        self.y = y
+        return self.X, self.y
