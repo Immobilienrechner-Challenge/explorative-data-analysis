e07345e94b5043a70a606dbe7f1e8888920ec979
Gabriel Torres Gamez <gabriel.torres@fuet.ch>
2022-10-12 10:03:14 +0200

    extend helper class with prediction function

--- helper.py
+++ helper.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn.preprocessing import Normalizer
 
+
 class ImmoHelper(object):
     def __init__(self, url="https://raw.githubusercontent.com/Immobilienrechner-Challenge/data/main/immoscout_cleaned_lat_lon_fixed_v9.csv", type="csv"):
         self.X = None
@@ -9,7 +10,45 @@ class ImmoHelper(object):
         if type == "csv":
             self.data = pd.read_csv(url, low_memory=False)
 
-    def process_data(self, data = None):
+    def process_prediction(self, living_space, type, rooms, gde_tax):
+        col = [
+            'Living space',
+            'rooms',
+            'gde_tax',
+            'type_attic-flat',
+            'type_attic-room',
+            'type_castle',
+            'type_chalet',
+            'type_detached-house',
+            'type_detached-secondary-suite',
+            'type_duplex-maisonette',
+            'type_farmhouse',
+            'type_flat',
+            'type_furnished-residential-property',
+            'type_loft',
+            'type_penthouse',
+            'type_rustico',
+            'type_secondary-suite',
+            'type_semi-detached-house',
+            'type_single-room',
+            'type_stepped-apartment',
+            'type_stepped-house',
+            'type_studio',
+            'type_terrace-house',
+            'type_villa'
+        ]
+
+        input = {
+            "Living space": living_space,
+            f"type_{type}": 1,
+            "rooms": rooms,
+            "gde_tax": gde_tax
+        }
+        data = pd.DataFrame(input, columns=col, index=[0])
+        data = data.fillna(0)
+        return data.values
+
+    def process_data(self, data=None):
         if data == None:
             data = self.data.copy()
 
@@ -24,13 +63,14 @@ class ImmoHelper(object):
         data = data[cols]
 
         # Relevante Spalten verarbeiten
-        data["Living space"] = data["Living space"].str.replace("m²", "").astype(float)
+        data["Living space"] = data["Living space"].str.replace(
+            "m²", "").astype(float)
         data = pd.get_dummies(data, columns=["type"])
         data = data.dropna()
 
         # X und y definieren und X normalisieren
         y = data["price_cleaned"].values
-        X = data.drop(columns = ["price_cleaned"]).values
+        X = data.drop(columns=["price_cleaned"]).values
         X = Normalizer().transform(X)
 
         self.data = data
