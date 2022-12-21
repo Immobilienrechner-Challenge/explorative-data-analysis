87670b0757916190adadf08a879d426534aa1f45
Gabriel Torres Gamez <gabriel.torres@fuet.ch>
2022-12-21 15:38:33 +0100

    update kaggle cleaning

--- utils/clean_data_kaggle.py
+++ utils/clean_data_kaggle.py
@@ -1,12 +1,11 @@
 # import helper functions
 from helper_kaggle import ImmoHelper
-import eda_generate_sweetviz_reports_new
 
 helper = ImmoHelper(url="../data/kaggle_uncleaned.csv", type="csv")
+helper.data.to_parquet("../data/kaggle_uncleaned.parquet")
+
 df = helper.process_data(return_gde=False)
-df.to_csv("../data/kaggle_cleaned.csv")
+df.to_parquet("../data/kaggle_cleaned.parquet")
 
 df_gde = helper.process_data(return_gde=True)
-df_gde.to_csv("../data/kaggle_gde_cleaned.csv")
-
-# eda_generate_sweetviz_reports_new.generate_sweetviz_report()
+df_gde.to_parquet("../data/kaggle_gde_cleaned.parquet")
